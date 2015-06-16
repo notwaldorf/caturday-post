276968a900a40968f16479467225c9b6f8eba526
Monica Dinculescu <noms@google.com>
2015-06-16 00:56:13 -0700

    😻~~random catsss~~🙀

--- server.js
+++ server.js
@@ -15,6 +15,34 @@ var server = app.listen(3000, function () {
   console.log('Started server at http://%s:%s', server.address().address, server.address().port);
 });
 
+
+// This is what <platinum-push-messaging> uses as the notification content,
+// so we should intercept it and do something better with it. Like get it from
+// a giant cat server.
+app.get('/notification-data.json', function (req, res) {
+  // Testing data
+  var titles = ["I am a server cat",
+                "Halp I am a cat trapped in a push notification",
+                "Meow"];
+  var messages = ["I live in a server",
+                  "Hello? Is anyody there? Hello?",
+                  "Thank you for subscribing to cat facts"];
+  var icons = ["http://i.imgur.com/fRIM0VX.png",
+               "http://i.imgur.com/jtKsmWk.gif",
+               "http://i.imgur.com/PZr3RGC.jpg"];
+
+  var index = Math.floor(Math.random() * 3);
+
+  res.json({
+    'title': titles[index],
+    'message': messages[index],
+    'url': icons[index],
+    'icon': icons[index],
+    'tag': 'cat-push-notification'
+  });
+});
+
+// Add or remove a client.
 app.post('/subscription_change', function (req, res) {
   var enabled = req.body.enabled;
   var id = req.body.id;
@@ -34,18 +62,22 @@ app.get('/get_subscription_count', function (req, res) {
   res.json({'subscriptions': activeSubscriptionIds.length});
 });
 
+// Send cats to everyone!!
 app.get('/push_cats', function (req, res) {
   var data = {
     "delayWhileIdle":true,
     "timeToLive":3,
+    "data":{
+      'title': 'this is an important cat notification',
+      'message': 'click on it. click on the cat.'
+    },
     "registration_ids":activeSubscriptionIds
   };
 
   var dataString =  JSON.stringify(data);
   var headers = {
     'Authorization' : 'key=' + config.apiKey,
-    'Content-Type' : 'application/json',
-    'Content-Length' : dataString.length
+    'Content-Type' : 'application/json'
   };
 
   var options = {
