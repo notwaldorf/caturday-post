d60e44940db77ea24546033a8fb1262904260af0
Monica Dinculescu <noms@google.com>
2015-06-15 23:19:56 -0700

    🚜  track subscribers

--- sender.js
+++ sender.js
@@ -1,15 +1,35 @@
-var express = require('express');
-var http = require('http');
-
+var express    = require('express');
+var http       = require('http');
+var bodyParser = require("body-parser");
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+var activeSubscriptionIds = [];
 
 var server = app.listen(3000, function () {
   console.log('Started server at http://%s:%s', server.address().address, server.address().port);
 });
 
 app.post('/subscription_change', function (req, res) {
-  console.log(req.query['id']);
-  //console.log("received", req);
+  var enabled = req.body.enabled;
+  var id = req.body.id;
+  var index = activeSubscriptionIds.indexOf(id);
+
+  if (enabled == 'true') {
+    if (index == -1)
+      activeSubscriptionIds.push(id);
+  } else {
+    var index = activeSubscriptionIds.indexOf(id)
+    activeSubscriptionIds.splice(index,1);
+  }
+
+  console.log(activeSubscriptionIds);
+  res.end("yes");
+});
+
+app.get('/get_subscription_count', function (req, res) {
+  res.json({'subscriptions': activeSubscriptionIds.length});
 });
