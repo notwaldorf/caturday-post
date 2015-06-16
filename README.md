607631db14b3f311279cc0a07616e71339b6de56
Monica Dinculescu <noms@google.com>
2015-06-15 23:46:24 -0700

    😹📢👏 kinda works!

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # push-cat-ifications
 Push notifications with cats
+
+
+# Configure it
+You need to add two files:
+
+- a `config.js` file in the root of your project, that contains your api key. It
+should look something like this:
+
+```
+module.exports = {'apiKey': 'YOUR_SUPER_SECRET_API_KEY'};
+```
+- a `manifest.json` file in the `public` directory, that contains your GCM
+project id. It should look something like this:
+
+```
+{
+	"gcm_sender_id": "YOUR_GCM_PROJECT_NUMBER",
+	"gcm_user_visible_only": true
+}
+
+```
+
+# Install it
+This uses `npm` for the node server bits, and `bower` for the client side
+polymer bits (in particular `platinum-push-messaging`, without which I would
+have had to slave for days over service worker code. 💖)
+
+```
+npm install
+bower install
+```
+
+# Run it
+You start the server with
+
+```
+node server.js
+```
+
+In your browser, open two tabs:
+
+- a [page](http://localhost:3000/index.html) that lets you send notifications to all the clients. It should tell you how many clients are registered.
+- a [page](http://localhost:3000/registration.html) that lets the browser register for push notifications. You will have to enable push notifications for
+this to work.
+
+Then watch the cats roll in.
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express    = require('express');
 var http       = require('http');
 var bodyParser = require("body-parser");
+var config = require('./config')
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,11 +27,52 @@ app.post('/subscription_change', function (req, res) {
     var index = activeSubscriptionIds.indexOf(id)
     activeSubscriptionIds.splice(index,1);
   }
-
-  console.log(activeSubscriptionIds);
   res.end("yes");
 });
 
 app.get('/get_subscription_count', function (req, res) {
   res.json({'subscriptions': activeSubscriptionIds.length});
+});
+
+app.get('/push_cats', function (req, res) {
+  var data = {
+    "delayWhileIdle":true,
+    "timeToLive":3,
+    "registration_ids":activeSubscriptionIds
+  };
+
+  var dataString =  JSON.stringify(data);
+  var headers = {
+    'Authorization' : 'key=' + config.apiKey,
+    'Content-Type' : 'application/json',
+    'Content-Length' : dataString.length
+  };
+
+  var options = {
+    host: 'android.googleapis.com',
+    port: 80,
+    path: '/gcm/send',
+    method: 'POST',
+    headers: headers
+  };
+
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf-8');
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+    res.on('end', function() {
+      console.log(responseString);
+    });
+    console.log('STATUS: ' + res.statusCode);
+  });
+  req.on('error', function(e) {
+    console.log('error : ' + e.message + e.code);
+  });
+
+  req.write(dataString);
+  req.end();
+
 });
